c0125b8ef03fd7e56104fd5fb33c568a80741555
Edluel <edluel_machado@hotmail.com>
2023-01-29 22:05:21 -0300

    fix @ appearing in all comments and identifing answers to comments

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,66 @@
-const data = {
-    "comments": [
-      {
-        "id": 1,
-        "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-        "createdAt": "1 month ago",
-        "score": 12,
-        "user": {
-          "image": { 
-            "png": "assets/images/avatars/image-amyrobson.png",
-            "webp": "assets/images/avatars/image-amyrobson.webp"
-          },
-          "username": "amyrobson"
-        },
-        "replies": []
-      },
-      {
-        "id": 2,
-        "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-        "createdAt": "2 weeks ago",
-        "score": 5,
-        "user": {
-          "image": { 
-            "png": "assets/images/avatars/image-maxblagun.png",
-            "webp": "assets/images/avatars/image-maxblagun.webp"
-          },
-          "username": "maxblagun"
-        },
-        "replies": [
-          {
-            "id": 3,
-            "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-            "createdAt": "1 week ago",
-            "score": 4,
-            "replyingTo": "maxblagun",
-            "user": {
-              "image": { 
-                "png": "assets/images/avatars/image-ramsesmiron.png",
-                "webp": "assets/images/avatars/image-ramsesmiron.webp"
-              },
-              "username": "ramsesmiron"
-            }
-          },
-          {
-            "id": 4,
-            "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-            "createdAt": "2 days ago",
-            "score": 2,
-            "replyingTo": "ramsesmiron",
-            "user": {
-              "image": { 
-                "png": "assets/images/avatars/image-juliusomo.png",
-                "webp": "assets/images/avatars/image-juliusomo.webp"
-              },
-              "username": "juliusomo"
-            }
-          }
-        ]
-      }
-    ]
-}
-
 function addComment(){
+    // Data to frist load
+    const data = {
+        "comments": [
+        {
+            "id": 1,
+            "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+            "createdAt": "1 month ago",
+            "score": 12,
+            "user": {
+            "image": { 
+                "png": "assets/images/avatars/image-amyrobson.png",
+                "webp": "assets/images/avatars/image-amyrobson.webp"
+            },
+            "username": "amyrobson"
+            },
+            "replies": []
+        },
+        {
+            "id": 2,
+            "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+            "createdAt": "2 weeks ago",
+            "score": 5,
+            "user": {
+            "image": { 
+                "png": "assets/images/avatars/image-maxblagun.png",
+                "webp": "assets/images/avatars/image-maxblagun.webp"
+            },
+            "username": "maxblagun"
+            },
+            "replies": [
+            {
+                "id": 3,
+                "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+                "createdAt": "1 week ago",
+                "score": 4,
+                "replyingTo": "maxblagun",
+                "user": {
+                "image": { 
+                    "png": "assets/images/avatars/image-ramsesmiron.png",
+                    "webp": "assets/images/avatars/image-ramsesmiron.webp"
+                },
+                "username": "ramsesmiron"
+                }
+            },
+            {
+                "id": 4,
+                "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+                "createdAt": "2 days ago",
+                "score": 2,
+                "replyingTo": "ramsesmiron",
+                "user": {
+                "image": { 
+                    "png": "assets/images/avatars/image-juliusomo.png",
+                    "webp": "assets/images/avatars/image-juliusomo.webp"
+                },
+                "username": "juliusomo"
+                }
+            }
+            ]
+        }
+        ]
+    }
     //Cloning template and getting template classes
     const template = document.getElementById('comment-template');
     const commentsSection = document.getElementById('comments-section');
@@ -84,12 +84,15 @@ function addComment(){
         if (data.comments[i].user.username !== "juliusomo") {
             const rmDel = clone.querySelector('.delete');
             const rmEdt = clone.querySelector('.edit');
+            const rmYou = clone.querySelector('.user-flag');
             rmDel.remove();
             rmEdt.remove();
+            rmYou.remove();
         }
         else {
             const rmRpl = clone.querySelector('.reply');
             rmRpl.remove();
+
         }
 
         // Add to DOM
@@ -100,23 +103,29 @@ function addComment(){
             for (let j = 0; j < data.comments[i].replies.length; j++) {
                 // Get template
                 const clone = document.importNode(template.content, true);
+                clone.firstElementChild.classList.add("reply-comment");
                 const avatar = clone.querySelector('.avatar');
                 const username = clone.querySelector('.username');
                 const at = clone.querySelector('.when');
                 const content = clone.querySelector('.content');
                 const score = clone.querySelector('.counter');
+                const reply = clone.querySelector('.replyingTo');
                 // Set propoerties
                 avatar.src = data.comments[i].replies[j].user.image.png;
                 username.innerHTML = data.comments[i].replies[j].user.username;
                 at.innerHTML = data.comments[i].replies[j].createdAt;
                 content.innerHTML = data.comments[i].replies[j].content;
                 score.innerHTML = data.comments[i].replies[j].score;
+                reply.innerHTML = '@';
+                reply.innerHTML += data.comments[i].replies[j].replyingTo;
                 // Choose controls
                 if (data.comments[i].replies[j].user.username !== "juliusomo") {
                     const rmDel = clone.querySelector('.delete');
                     const rmEdt = clone.querySelector('.edit');
+                    const rmYou = clone.querySelector('.user-flag');
                     rmDel.remove();
                     rmEdt.remove();
+                    rmYou.remove();
                 }
                 else {
                     const rmRpl = clone.querySelector('.reply');
@@ -128,6 +137,4 @@ function addComment(){
         }
     }
 }
-
 addComment();
-
