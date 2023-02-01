95b2705e46ae6ca2f4d9cd1251b6d31aac12fe77
Edluel <edluel_machado@hotmail.com>
2023-01-31 21:05:32 -0300

    Added send/reply/edit/delete functions

--- assets/script.js
+++ assets/script.js
@@ -67,6 +67,19 @@ function addComment(){
             "username": "juliusomo"
             },
             "replies": []
+        },
+        {
+            "content": "When we hit the lack of knowledge, it's the best opportunity to improve our skills. That's why I like these challenges.",
+            "createdAt": "2 days ago",
+            "score": 142,
+            "user": {
+            "image": { 
+                "png": "assets/images/avatars/image-amyrobson.png",
+                "webp": "assets/images/avatars/image-amyrobson.webp"
+            },
+            "username": "amyrobson"
+            },
+            "replies": []
         }
         ]
     }
@@ -149,7 +162,7 @@ function addComment(){
             }  
         }
     }
-}
+} //add comments from assets
 addComment();
 
 function scoreIterator(e){
@@ -177,7 +190,7 @@ function scoreIterator(e){
 
     // Update counter
     counter.textContent = value;
-}
+} // change upvotes and downvotes
 
 function deleteComment(e) {
     // Change focus to delete screen
@@ -218,7 +231,7 @@ function deleteComment(e) {
     
 
 
-}
+} // remove comment
 
 function commentOnPost(event){
     event.preventDefault();
@@ -252,8 +265,125 @@ function commentOnPost(event){
     const rmRpl = clone.querySelector('.reply');
     rmRpl.remove();
     
-    // Add to DOM
-    commentsSection.appendChild(clone);
+    // If it contains text Add to DOM
+    if (comment){
+        commentsSection.appendChild(clone);
+    }
+    
+} // comment on main
 
+function answerComment(e){
+    // Get clicked element
+    const click = e.target;
+    let parent;
+    if (click.closest('.reply-comment')) {
+        parent = click.closest('.reply-comment');
+
+    } else {
+        parent = click.closest('.comment');
+    }
+
+    //Cloning template and getting template classes
+    const template = document.getElementById('comment-template');
+    const commentsSection = document.getElementById('comments-section');
+
+    const clone = document.importNode(template.content, true);
+    clone.firstElementChild.classList.add("reply-comment");
+    
+    const avatar = clone.querySelector('.avatar');
+    const username = clone.querySelector('.username');
+    const at = clone.querySelector('.when');
+    const content = clone.querySelector('.content');
+    const score = clone.querySelector('.counter');
+    const fix = clone.querySelector('.counter-fix');
+    const reply = clone.querySelector('.replyingTo');
+    
+    // Set propoerties
+    avatar.src = 'assets/images/avatars/image-juliusomo.png';
+    username.innerHTML = 'juliusomo';
+    at.innerHTML = 'now';
+    reply.innerHTML = '@';
+    reply.innerHTML += parent.querySelector('.username').textContent;
+    score.innerHTML = 0;
+    fix.innerHTML = 0;
+
+    // Remove reply control
+    const rmRpl = clone.querySelector('.reply');
+    rmRpl.remove();
+
+    // Change button to reply
+    let sendBt = document.querySelector('.send-comment-bt');
+    sendBt.style.display = 'none';
+    let replyBt = document.querySelector('.send-answer-bt');
+    replyBt.style.display = 'block';
+
+    // Add reply sender
+    replyBt.addEventListener('click', sendAnswer);
+    function sendAnswer(event){
+        event.preventDefault();
+        // Form values
+        const form = document.getElementById('comment-form');
+        const comment = document.querySelector("textarea").value;
+        form.reset();
+        content.innerHTML += comment;
+        // If it contains text Add to DOM
+        if (comment){
+            commentsSection.appendChild(clone);
+        }
+        // Change button back ro send
+        replyBt.style.display = 'none';
+        sendBt.style.display = 'block';
+        // Discard class changes
+        clone.firstElementChild.classList.remove("reply-comment");
+    }
+
+}
+
+function editComment(e) {
+    // Get clicked element
+    const click = e.target;
+    let parent;
+    if (click.closest('.reply-comment')) {
+        parent = click.closest('.reply-comment');
+
+    } else {
+        parent = click.closest('.comment');
+    }
+
+    // Get class to edit comment
+    let edit = parent.querySelector('.content');
+    let reply = parent.querySelector('.replyingTo').textContent;
+
+    // Change button to edit
+    let sendBt = document.querySelector('.send-comment-bt');
+    sendBt.style.display = 'none';
+    let editBt = document.querySelector('.send-edit-bt');
+    editBt.style.display = 'block';
+
+    // Add reply sender
+    editBt.addEventListener('click', sendAnswer);
+    function sendAnswer(event){
+        event.preventDefault();
+
+        // Form values
+        const form = document.getElementById('comment-form');
+        const comment = document.querySelector("textarea").value;
+        form.reset();
+
+        // If it contains text Add to DOM
+        if (comment){
+            if (click.closest('.reply-comment')) {
+                edit.innerHTML = reply + ' ';
+                edit.innerHTML += comment;
+            } else {
+                edit.innerHTML = comment;
+            }
+
+        }
+
+        // Change button back ro send
+        editBt.style.display = 'none';
+        sendBt.style.display = 'block';
+    }
 }
 
